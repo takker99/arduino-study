7cac93456fd159af398c3059b74bd20076d63d42
takker99 <37929109+takker99@users.noreply.github.com>
2025-07-19 16:32:05 +0900

    feat: add Arduino LED Matrix example implementation

---
+++ r4-led/main.cpp
@@ -0,0 +1,79 @@
+// slightly modified version of the [Arduino LED Matrix example](https://docs.arduino.cc/tutorials/uno-r4-wifi/led-matrix/)
+//
+// Copyright (c) 2024, The Arduino Project. All rights reserved.
+
+#include "Arduino_LED_Matrix.h"
+
+ArduinoLEDMatrix matrix;
+
+void setup()
+{
+  Serial.begin(9600);
+  matrix.begin();
+}
+
+uint8_t frame[8][12] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+void leftEye()
+{
+  // Left eye
+  frame[1][3] = 1;
+  frame[1][4] = 1;
+  frame[2][3] = 1;
+  frame[2][4] = 1;
+}
+
+void wink()
+{
+  // Wink with the left eye
+  frame[1][3] = 0;
+  frame[1][4] = 0;
+  frame[2][3] = 1;
+  frame[2][4] = 1;
+}
+
+void rightEye()
+{
+  // Right eye
+  frame[1][8] = 1;
+  frame[1][9] = 1;
+  frame[2][8] = 1;
+  frame[2][9] = 1;
+}
+
+void mouth()
+{
+  // Mouth
+  frame[5][3] = 1;
+  frame[5][9] = 1;
+  frame[6][3] = 1;
+  frame[6][4] = 1;
+  frame[6][5] = 1;
+  frame[6][6] = 1;
+  frame[6][7] = 1;
+  frame[6][8] = 1;
+  frame[6][9] = 1;
+}
+
+void loop()
+{
+  leftEye();
+  rightEye();
+  mouth();
+
+  matrix.renderBitmap(frame, 8, 12);
+
+  delay(1000);
+  wink();
+
+  matrix.renderBitmap(frame, 8, 12);
+  delay(1000);
+}
